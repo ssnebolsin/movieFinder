8802637ee8592ce6e86b292bf3783b6f865e3fb2
Serhii Nebolsin <SNebolsin_c@spherasolutions.com>
2024-02-13 21:49:54 +0200

    history items

--- js/main.js
+++ js/main.js
@@ -3,28 +3,24 @@ const queryField = document.querySelector('.input')
 let counter = 0
 let results = document.querySelector('.search-results')
 let loaded = false
+let historyItemsList = []
+
+
+
+window.onload = () => {
+    localStorageGetHistoryItems ();
+  };
 
 queryField.addEventListener('input', function(e){
-    // e.preventDefault()
 
     let query = e.target.value
-
-    // searchMovie(query.value)
-    // getMovie(query, 500)
-
-    debounce(query, 500)
-
-    // document.querySelector('.search-results__poster').setAttribute('src', movieInfo[0].Poster)
-    // document.querySelector('.search-results__title').setAttribute('src', movieInfo[0].Title)
-
+    debounce(query, 1000)
 
 })
-
 
 async function searchMovie(query){
     let res = await fetch(`${BASE_URL}s=${query}`)
     let data = await res.json()
-    // console.log(data.Search)
     if(data.Search){
         data.Search.forEach(item => {
         results.appendChild(renderSearchItem(item))
@@ -41,14 +37,15 @@ function debounce (val, timeout) {
 
     if (val.length > 0) {
         timeOutFunc = setTimeout( ()=>{
-        console.log(val)
-        searchMovie(val)
-        loaded = true
-        // fillUpInfo(movieData)
-        // renderSearchItem()
-    }, timeout)
-    }
+            console.log(val)
+            searchMovie(val)
+            loaded = true
+            chatSocket.send(val)
+            history.innerHTML += showItem(val)
+            localStorageSetHistoryItem(val)
 
+        }, timeout)
+    }
 }
 
 
@@ -61,32 +58,75 @@ function renderSearchItem(item){
     poster.innerHTML = `<img src="${item.Poster}" alt="${item.Title}"></img>`
     wrap.appendChild(poster)
 
+    let info = document.createElement('div')
+    info.classList.add('search-results__data')
+    wrap.appendChild(info)
+
     let title = document.createElement('h2')
     title.classList.add('search-results__title')
     title.innerText = item.Title
-    wrap.appendChild(title)
+    info.appendChild(title)
 
-    let about = document.createElement('p')
-    about.classList.add('search-results__about')
-    about.innerHTML += `<span class "search-results__year"> ${item.Year}</span>`
-    about.innerHTML += `<span class "search-results__type"> ${item.Type}</span>`
-    wrap.appendChild(about)
+    info.innerHTML += `<p class = "search-results__type"> Type: ${item.Type}</p>`
+    info.innerHTML += `<p class = "search-results__year"> Year: ${item.Year}</p>`
 
     let link = document.createElement('a')
+    link.classList.add('search-results-link')
     link.setAttribute('href', `single.html?i=${item.imdbID}`)
     link.innerText = 'About'
-    link.innerHTML +=  `<span class="material-symbols-outlined">arrow_forward_ios</span>`
-    wrap.appendChild(link)
+    info.appendChild(link)
 
     return wrap
 }
 
 document.addEventListener('scroll', ()=> {
-    // let mainHeight = document.querySelector('.search-results').clientHeight
     if (document.querySelector('.search-results').getBoundingClientRect().bottom - 100 < window.innerHeight && loaded === true){
 
         console.log('start func')
         loaded = false
 
     }
+})
+
+// ######### Sockets ###########
+const history = document.querySelector('.requests-history')
+
+const wsUri = 'wss://socketsbay.com/wss/v2/1/d0173ca5185ec7768d52d25fc4c277b7/'
+
+let chatSocket = new WebSocket(wsUri)
+
+chatSocket.onopen = function (){
+    console.log('socket is open')
+}
+
+chatSocket.onmessage = function (event){
+    history.innerHTML += showItem(event.data)
+}
+
+function showItem(item){
+    console.log(item)
+    return `<div class="requests-history-item">${item}</div>`
+}
+
+function localStorageSetHistoryItem(item){
+    historyItemsList = localStorage.getItem('historyItems')
+    historyItemsList = historyItemsList.split(',')
+    historyItemsList.push(item)
+    localStorage.setItem('historyItems', historyItemsList)
+}
+
+function localStorageGetHistoryItems (){
+    let histItems = localStorage.getItem('historyItems')
+    histItems.split(',').forEach(item => {
+        history.innerHTML += showItem(item)
+    })
+}
+
+let historyAll = document.querySelectorAll('.requests-history')
+
+historyAll.forEach((item)=> {
+    item.addEventListener('click', function(e){
+        console.log(e.target.innerText)
+        searchMovie(e.target.innerText)
+    })
 })
